6d15d749ccc1c21d22f3886ad3180925dc444697
10522043-Phat <10522043@student.vgu.edu.vn>
2026-06-04 22:32:28 +0700

    Bot đóng tiền AI

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN      = process.env.VERIFY_TOKEN || "mytoken123";
 const MONGODB_URI       = process.env.MONGODB_URI;
 const QR_CODE_URL       = "https://i.ibb.co/9m1Np9PY/tenfile.jpg";
-const DANH_SACH_TEN_URL ="https://i.ibb.co/s4PVx01/tenfile.jpg";
+const DANH_SACH_TEN_URL ="https://i.ibb.co/7dTD0tYm/danh_sach_ten.png";
 let ALLOWED_NAMES = [
   "Quyên", "Trúc Ngân", "Thiên An", "Hao Huynh",
   "Trần Agness", "Bảo Duy", "Ryan Nguyen", "Gia Bảo",
