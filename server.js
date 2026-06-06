1d3d89458684e32d7fa4e5b224195f410ef3eae3
10522043-Phat <10522043@student.vgu.edu.vn>
2026-06-06 13:44:47 +0700

    Bot đóng tiền AI

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ async function xuLyGetStarted(senderId) {
 }
  
 // ===== LỊCH NHẮC ĐÓNG TIỀN =====
-cron.schedule("39 13 6 * *", async () => {
+cron.schedule("46 13 6 * *", async () => {
   console.log("Gửi nhắc đóng tiền");
   const users = await User.find({ xacNhan: true });
 
